479fd68045f9b6bb0ebfa32ac95a31c75c21710d
Dominik Kunz <dominik.kunz@gmail.com>
2019-01-04 20:08:19 +0100

    first commit

---
+++ src/lt_dash.h
@@ -0,0 +1,14 @@
+#ifndef LT_DASH_H
+#define LT_DASH_H
+
+#define DHTPIN 2
+#define DHTTYPE 22
+
+#define STATE_COUNT 2
+#define STATE_INIT -1
+#define STATE_TEMPERATURE 0
+#define STATE_ENGINE 1
+
+#define DISPLAY_SWITCH_INTERVAL 5000
+
+#endif
